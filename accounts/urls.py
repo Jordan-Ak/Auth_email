8e9ae82afb81bbcb88c45ce0d1ef2f0522d3e2f8
Jordan Akpoborie <JosiahDavid98@gmail.com>
2021-05-12 09:36:05 +0100

    Swagger auto schema partial-remove under review.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,20 +1,22 @@
 
 from django.urls import path
-from accounts.views import (ListUsersView, CurrentUserView, PasswordResetConfirmView, PasswordResetSendView,
-                            SignUpView, UserDeleteView, UserPasswordChangeView,
-                            UserEmailVerificationView,
-                            UserResendEmailVerificationView)
+from accounts.views import (CurrentUserView, 
+                            ListUsersView, PasswordResetConfirmView, PasswordResetSendView,
+                            SignUpView, 
+                            UserDeleteView, UserEmailVerificationView, 
+                            UserPasswordChangeView, UserResendEmailVerificationView,) 
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('', ListUsersView.as_view(), name = 'list-users'),
     path('signup/', SignUpView.as_view(), name = 'sign-up'),
+
     path('@me/<str:email>/', CurrentUserView.as_view(), name = 'current-user'),
     path('destroy/<str:email>/', UserDeleteView.as_view(), name = 'user-delete'),
     path('password/change/<str:id>/', UserPasswordChangeView.as_view(), name = 'password-change'),
     path('verify_mail/<str:email_verification_token>/', UserEmailVerificationView.as_view(),
-                                                    name = 'email-verify'),
+                                                   name = 'email-verify'),
     path('resend_mail/<str:id>/', UserResendEmailVerificationView.as_view(),
                                                    name = 'email-resend'), 
     path('password/reset/', PasswordResetSendView.as_view(), name = 'password-reset'),
